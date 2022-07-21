be2e8e3d85c96708708e907acafb963ed56c2e9c
jan-martinek <jan-martinek@users.noreply.github.com>
2022-07-21 14:53:46 +0000

    Deploying to docs from @ books-are-next/zabransky-za-alpami@db2dac0f2b36161024a7bc583b086a6aad33875e 🚀

--- epub2nb-output/content/about.md
+++ epub2nb-output/content/about.md
@@ -8,7 +8,7 @@ Redakce Jaroslava Bednářová
 Vydala Městská knihovna v Praze  
 Mariánské nám. 1, 115 72 Praha 1  
 ISBN 978-80-274-1602-8 (webová kniha)  
-V MKP 2. elektronické vydání z 7. 7. 2022.
+V MKP 2. elektronické vydání z 21. 7. 2022.
 
 ***
 
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-d0f2c00';
+const CACHE = 'cache-db2dac0';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
